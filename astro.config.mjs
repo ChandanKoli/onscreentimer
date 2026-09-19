893d09846478902fdc5fd3c69220dfed66813d3e
ChandanKoli <cmk142006@gmail.com>
2026-09-19 11:17:58 +0530

    chore: configure automated XML sitemap generation and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,14 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'https://onscreentimer.com',
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [sitemap()]
 });
